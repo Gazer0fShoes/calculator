9c1473ff32e2953984f919f4ea3eb8719369e8b9
gazer0fShoes <oisin.weitzman@gmail.com>
2024-07-02 15:42:18 +0900

    function now does not take more than one operator
    still does not calculate on entry of the second operator however.
    this is the next thing to tackle.

--- calculator.js
+++ calculator.js
@@ -2,12 +2,22 @@ let additionSymbol = document.querySelector("#addition").innerHTML;
 let subtractionSymbol = document.querySelector("#subtraction").innerHTML;
 let multiplicationSymbol = document.querySelector("#multiplication").innerHTML;
 let divisionSymbol = document.querySelector("#division").innerHTML;
+let numbers = "1234567890.";
+let operators = [additionSymbol, subtractionSymbol, multiplicationSymbol, divisionSymbol];
 const displayValue = document.getElementById("display");
 
-const basicButtons = document.querySelectorAll(".number-button, .operator-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        lastChar = displayValue.innerHTML.slice(-1);
+        if (!numbers.includes(lastChar)) displayValue.innerHTML = displayValue.innerHTML.slice(0, -1);
+    })
+})
+
+const basicButtons = document.querySelectorAll(".number-button, .operator-button, #decimal-button");
 basicButtons.forEach(button => {
     button.addEventListener("click", () => {
-        clickNumberButton(button.innerHTML)
+        enterExpression(button.innerHTML)
     })
 });
 
@@ -23,25 +33,30 @@ backspaceButton.addEventListener("click", () => {
 
 const equalsButton = document.querySelector("#equals-button");
 equalsButton.addEventListener("click", () => {
-    let acceptedChars = "1234567890."
-    let expression = displayValue.innerHTML;
+    expression = displayValue.innerHTML;
     let firstChar = expression.slice(0, 1);
     let lastChar = expression.slice(-1);
-    if (acceptedChars.includes(firstChar) && acceptedChars.includes(lastChar)) {
-        let operator = Array.from(expression).filter(char => !acceptedChars.includes(char)).join("");
-        console.log(operator);
-        let num1 = +expression.slice(0, expression.indexOf(operator));
-        let num2 = +expression.slice(expression.indexOf(operator) + 1);
-        displayValue.innerHTML = calculate(num1, num2, operator);
+    if (numbers.includes(firstChar) && numbers.includes(lastChar)) {
+        displayValue.innerHTML = calculate(expression);
     }
 });
 
 // FUNCTIONS
-function clickNumberButton(number) {
-    displayValue.innerHTML += number;
+function enterExpression(char) {
+    for (operator of operators) {
+        if (operators.includes(char) && displayValue.innerHTML.includes(operator)) {
+            return;
+        }
+    }
+
+    displayValue.innerHTML += char;
 }
 
-function calculate(num1, num2, operator) {
+function calculate(expression) {
+    let operator = Array.from(expression).filter(char => !numbers.includes(char)).join("");
+    let num1 = +expression.slice(0, expression.indexOf(operator));
+    let num2 = +expression.slice(expression.indexOf(operator) + 1);
+
     if (operator === additionSymbol) return add(num1, num2);
     if (operator === subtractionSymbol) return subtract(num1, num2);
     if (operator === multiplicationSymbol) return multiply(num1, num2);
