ac1b70e011bc9ce822ddc8381172b865a17f5b19
gazer0fShoes <oisin.weitzman@gmail.com>
2024-07-02 12:25:07 +0900

    implement operator functions
    will have to add exception handling for division by zero.

--- calculator.js
+++ calculator.js
@@ -21,27 +21,41 @@ backspaceButton.addEventListener("click", () => {
 const equalsButton = document.querySelector("#equals-button");
 equalsButton.addEventListener("click", () => {
     let expression = displayValue.innerHTML;
+    let additionSymbol = document.querySelector("#addition").innerHTML;
+    let subtractionSymbol = document.querySelector("#subtraction").innerHTML;
+    let multiplicationSymbol = document.querySelector("#multiplication").innerHTML;
+    let divisionSymbol = document.querySelector("#division").innerHTML;
+
     // ASSUME THERE WILL ONLY BE ONE OPERATOR
-    if (expression.slice(0, -1).includes(document.querySelector("#addition").innerHTML) 
-        && expression.slice(-1) >= "0" && expression.slice(-1) <= "9"
-        && expression.slice(0, 1) >= "0" && expression.slice(0, 1) <= "9") {
-        console.log("operator found. Function not yet implemented.");
+    if (expression.slice(-1) >= "0" && expression.slice(-1) <= "9"
+        && expression.slice(0, 1) >= "0" && expression.slice(0, 1) <= "9"
+    ){
+        if (expression.slice(0, -1).includes(additionSymbol)
+        ){
+            num1 = +expression.slice(0, expression.indexOf(additionSymbol));
+            num2 = +expression.slice(expression.indexOf(additionSymbol) + 1);
+            displayValue.innerHTML = add(num1, num2);
         };    
-    if (expression.slice(0, -1).includes(document.querySelector("#subtraction").innerHTML)
-        && expression.slice(-1) >= "0" && expression.slice(-1) <= "9"
-        && expression.slice(0, 1) >= "0" && expression.slice(0, 1) <= "9") {
-        console.log("operator found. Function not yet implemented.");
+        if (expression.slice(0, -1).includes(subtractionSymbol)
+        ){
+            num1 = +expression.slice(0, expression.indexOf(subtractionSymbol));
+            num2 = +expression.slice(expression.indexOf(subtractionSymbol) + 1);
+            displayValue.innerHTML = subtract(num1, num2);
         };    
-    if (expression.slice(0, -1).includes(document.querySelector("#multiplication").innerHTML)
-        && expression.slice(-1) >= "0" && expression.slice(-1) <= "9"
-        && expression.slice(0, 1) >= "0" && expression.slice(0, 1) <= "9") {
-        console.log("operator found. Function not yet implemented.");
+        if (expression.slice(0, -1).includes(multiplicationSymbol)
+        ){
+            num1 = +expression.slice(0, expression.indexOf(multiplicationSymbol));
+            num2 = +expression.slice(expression.indexOf(multiplicationSymbol) + 1);
+            displayValue.innerHTML = multiply(num1, num2);
         };    
-    if (expression.slice(0, -1).includes(document.querySelector("#division").innerHTML)
-        && expression.slice(-1) >= "0" && expression.slice(-1) <= "9"
-        && expression.slice(0, 1) >= "0" && expression.slice(0, 1) <= "9") {
-        console.log("operator found. Function not yet implemented.")
+        if (expression.slice(0, -1).includes(divisionSymbol)
+        ){
+            num1 = +expression.slice(0, expression.indexOf(divisionSymbol));
+            num2 = +expression.slice(expression.indexOf(divisionSymbol) + 1);
+            console.log(num1, num2);
+            displayValue.innerHTML = divide(num1, num2);
         };
+    }
 });
 
 // FUNCTIONS
